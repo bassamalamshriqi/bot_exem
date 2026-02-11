9d3437019716a59bc268deb49043495a26e28a7e
Bassam almashriqi <59414226+bassamalamshriqi@users.noreply.github.com>
2026-02-11 19:56:32 +0300

    Fix formatting and remove token from bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = "433265853:AAEjyWRYbleDzAfxHHI4SufJfzLt5kzq4b0"
+TOKEN = "AAEjyWRYbleDzAfxHHI4SufJfzLt5kzq4b0"
 
 questions = [
     {
@@ -51,5 +51,6 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, answer))
 
 app.run_polling()
+
 
 
